0a8aee8928201b9bf1280720c3f2c7a106453ff6
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-27 12:34:08 +0000

    added a main content file

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,10 @@
+export default function MainContent(): JSX.Element {
+  return (
+    <>
+      <h1>MAIN CONTENT</h1>
+      <svg width={250} height={250}>
+        <circle r={125} cx={125} cy={125} fill="blue" />
+      </svg>
+    </>
+  );
+}
