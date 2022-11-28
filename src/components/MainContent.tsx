dd7e26b7cd69e97f1bd046287b747d7e53f25d48
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-28 09:37:56 +0000

    created a CSS file and made the svg coloured so easy to easy

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,10 +1,26 @@
+import './MainContent.css'
+
 export default function MainContent(): JSX.Element {
+  const boxesToMake = [1, 2, 3, 4, 5, 6, 7]
+
+  /*
+  for each day of the array, create a rect element starting at x=3 y = i*20
+  */
   return (
     <>
       <h1>MAIN CONTENT</h1>
-      <svg width={250} height={250}>
-        <circle r={125} cx={125} cy={125} fill="blue" />
+      <svg width={1000} height={300} className="SVG">
+        {boxesToMake.map((day, i) => {return <rect width={15} height={15} x={30} y={(i*20) + 50} key={day} fill="grey"/>}
+        )}
       </svg>
     </>
   );
 }
+
+/* <rect width={15} height={15} x={30} y={10}/>
+<rect width={15} height={15} x={30} y={30}/>
+<rect width={15} height={15} x={30} y={50}/>
+<rect width={15} height={15} x={30} y={70}/>
+<rect width={15} height={15} x={30} y={90}/>
+<rect width={15} height={15} x={30} y={110}/>
+<rect width={15} height={15} x={30} y={130}/> */
