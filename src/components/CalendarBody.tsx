81712d46c3428552ccd2fd3f3b80c2997f00be37
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-12-15 23:24:03 +0000

    Created boxes for number of days in the current year so far

--- src/components/CalendarBody.tsx
+++ src/components/CalendarBody.tsx
@@ -1,14 +1,40 @@
 import "./CalendarBody.css";
+import { getDaysInYear, differenceInCalendarDays } from "date-fns";
 
 export default function MainContent(): JSX.Element {
-  const boxesToMake = [1, 2, 3, 4, 5, 6, 7];
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
 
-  /*
-  for each day of the array, create a rect element starting at x=3 y = i*20
-  */
+  const startOfYear = new Date(currentYear, 0, 1);
+
+  const daysInYearSoFar = differenceInCalendarDays(currentDate, startOfYear);
+
+  //I want boxes to have 7 rows
+  const boxesPerRow = 7;
+
+  //making boxes for each day of the year so far
+  const boxes = [];
+  for (let i = 0; i < daysInYearSoFar; i++) {
+    const xPosition = Math.floor(i / boxesPerRow) * 10;
+    const yPosition = (i % boxesPerRow) * 10;
+    boxes.push(
+      <rect
+        key={i}
+        x={xPosition}
+        y={yPosition}
+        width={10}
+        height={10}
+        fill="red"
+      ></rect>
+    );
+  }
+
   return (
     <>
       <h1>MAIN CONTENT</h1>
+      <svg width={1000} height={500} className="SVGBox">
+        {boxes}
+      </svg>
     </>
   );
 }
