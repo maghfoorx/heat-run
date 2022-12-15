714c5031ea95c59a8532e96253dd12ea458fa77f
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-12-15 22:37:52 +0000

    added prettier and changed workflows

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,7 +1,7 @@
-import './MainContent.css'
+import "./MainContent.css";
 
 export default function MainContent(): JSX.Element {
-  const boxesToMake = [1, 2, 3, 4, 5, 6, 7]
+  const boxesToMake = [1, 2, 3, 4, 5, 6, 7];
 
   /*
   for each day of the array, create a rect element starting at x=3 y = i*20
@@ -9,18 +9,6 @@ export default function MainContent(): JSX.Element {
   return (
     <>
       <h1>MAIN CONTENT</h1>
-      <svg width={1000} height={300} className="SVG">
-        {boxesToMake.map((day, i) => {return <rect width={15} height={15} x={30} y={(i*20) + 50} key={day} fill="grey"/>}
-        )}
-      </svg>
     </>
   );
 }
-
-/* <rect width={15} height={15} x={30} y={10}/>
-<rect width={15} height={15} x={30} y={30}/>
-<rect width={15} height={15} x={30} y={50}/>
-<rect width={15} height={15} x={30} y={70}/>
-<rect width={15} height={15} x={30} y={90}/>
-<rect width={15} height={15} x={30} y={110}/>
-<rect width={15} height={15} x={30} y={130}/> */
