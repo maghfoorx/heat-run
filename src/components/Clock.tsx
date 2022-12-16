bca10ca806f34390298659c3f03228aca5705dfa
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-12-16 12:17:39 +0000

    added timezone

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 export default function Clock(): JSX.Element {
   const [time, setTime] = useState(new Date());
+  console.log(time);
 
   useEffect(() => {
     const updateTime = setInterval(() => {
@@ -11,7 +12,14 @@ export default function Clock(): JSX.Element {
   });
   return (
     <>
-      <h3>The Time currenly is: {format(time, "HH:mm:ss")}</h3>
+      <h3>The Time currenly is: {format(time, "HH:mm:ss z")}</h3>
     </>
   );
 }
+
+/*
+Lesson about useEffect:
+You cannot just have your useEffect do setTime(new Date()) because then it will be an infinite loop and console starts giving you errors.
+Instead you should run a function that runs after every 1 second. Providing no 2nd argument makes the useEffect run on every render
+ so when the time state changes after the updateTime runs the useEffect will run and so it keeps running on every re render.
+*/
