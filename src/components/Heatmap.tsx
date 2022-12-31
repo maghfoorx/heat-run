4dc8a3886304a3ec26eeebe61b203217e121f7d8
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-12-31 16:27:27 +0000

    getting setup to learn d3

--- src/components/Heatmap.tsx
+++ src/components/Heatmap.tsx
@@ -1,37 +1,22 @@
-import { getDaysInYear, differenceInCalendarDays } from "date-fns";
+import { startOfYear, endOfYear, eachDayOfInterval, format, addYears, subDays } from 'date-fns';
+
+export interface DataForHeatmap {
+  date: Date;
+  color: "red" | "filled"
+}
+
 
 export default function Heatmap(): JSX.Element {
   const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
+  const startDate = subDays(startOfYear(addYears(currentDate, 1)), 1)
+  const endDate = endOfYear(addYears(currentDate, 1));
+  const dates = eachDayOfInterval({ start: startDate, end: endDate });
 
-  const startOfYear = new Date(currentYear, 0, 1);
-
-  const daysInYearSoFar = differenceInCalendarDays(currentDate, startOfYear);
-
-  //I want boxes to have 7 rows
-  const boxesPerRow = 7;
-
-  //making boxes for each day of the year so far
-  const boxes = [];
-  for (let i = 10; i < daysInYearSoFar + 10; i++) {
-    const xPosition = Math.floor(i / boxesPerRow) * 20;
-    const yPosition = (i % boxesPerRow) * 20 + 20;
-    boxes.push(
-      <circle key={i} cx={xPosition} cy={yPosition} r="8" fill="grey" />
-    );
-  }
+  
 
   return (
     <>
-      <h1>Running Heatmap</h1>
-      <p>
-        Currently, I have one circle plotted for each day in the year so far
-      </p>
-      <div className="SVGWrapper">
-        <svg width={1200} height={500} className="SVGBox">
-          {boxes}
-        </svg>
-      </div>
+    
     </>
   );
 }
--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -1,11 +1,14 @@
 import Clock from "./Clock";
 import Heatmap from "./Heatmap";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
 import "./styles.css";
 
 export default function MainBody(): JSX.Element {
     return (
         <>
         <Clock />
+        <Calendar />
         <Heatmap />
         </>
     )
