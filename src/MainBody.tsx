07e9048c6c58ab7b88bc0273061e5887b06c9014
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-01-08 14:36:15 +0000

    heatmap represents data fetched from API

--- src/MainBody.tsx
+++ src/MainBody.tsx
@@ -2,18 +2,31 @@ import { useEffect, useState } from "react";
 import ViewCalendar from "./ViewCalendar";
 import HeatMap from "./HeatMap";
 import Table from "./Table";
+import { APIDataType } from "./utils/DatesDataInterface";
+import { getRunningData } from "./utils/getRunningData";
+import Form from "./Form";
 
 export default function MainBody(): JSX.Element {
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+  const [runningData, setRunningData] = useState<APIDataType[]>([]);
+
+  useEffect(() => {
+    getRunningData().then((data) => {
+      setRunningData(data);
+    });
+  }, []);
 
   return (
     <>
-      <HeatMap selectedDate={selectedDate} />
+      <HeatMap selectedDate={selectedDate} runningData={runningData} />
+      <hr />
+      {/* <Form selectedDate={selectedDate}/> */}
+      <hr />
       <ViewCalendar
         setSelectedDate={setSelectedDate}
         selectedDate={selectedDate}
       />
-      <Table />
+      {/* <Table runningData={runningData}/> */}
     </>
   );
 }
