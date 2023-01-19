c329c456a2ae9700657565a4e1ce0db2a07abb52
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-01-19 21:34:18 +0000

    added react toastify and delete button

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,14 +1,35 @@
+import axios from "axios";
 import { format } from "date-fns";
+import { BaseURL } from "../utils/BaseURL";
 import { HeatmapDataType } from "../utils/DatesDataInterface";
 import "./popup.css";
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 interface PopupProps {
   popup: boolean;
   setPopup: React.Dispatch<React.SetStateAction<boolean>>;
   valueForPopup: null | HeatmapDataType;
+  setLogButtonClicked: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export default function PopupView(props: PopupProps): JSX.Element {
+
+
+  const handleDeleteLog = async () => {
+    try {
+      await axios.delete(`${BaseURL}/runs/${props.valueForPopup?.date}`,)
+      toast.success("Data deleted for this date!😁")
+      props.setLogButtonClicked((prev) => !prev)
+    }
+    catch (error) {
+      toast.error("Failed to delete data☹️")
+    }
+  }
+
+  const notifyButton = () => {
+    toast.error("it didnt work:(")
+  }
   return (
     <>
       <p>Hello</p>
@@ -28,9 +49,11 @@ export default function PopupView(props: PopupProps): JSX.Element {
             >
               Close
             </button>
+            <button className="delete-modal" onClick={handleDeleteLog}>Delete Log</button>
           </div>
         </div>
       )}
+      <ToastContainer autoClose={2000} />
     </>
   );
 }
